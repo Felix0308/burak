e15b815a04e67a58c6cb229e79f7cdf8609eb467
felix0308 <utkirbekm.0327@gmail.com>
2024-12-08 21:57:19 +0900

    Initial commit on develop branch

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@
 // const currentTime = moment().format("YYYY MM DD");
 // console.log(currentTime);
 
-
 // const person: string = "Martin";
 // const count: number = 100;
+
+// Architectural pattern: MVC, DI, MVP
+// Design pattern: Middleware, Decotar
