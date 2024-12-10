98bcb18ce0217c883d74656c2485ba78c4afd754
Felix0308 <utkirbekm.0327@gmail.com>
2024-12-10 20:26:39 +0900

    feat TASK-H

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+// **********************************************************************
+// TASK H:
+
+// Raqamlardan iborat arrayni argument sifatida qabul qiladigan
+// function tuzing. Ushbu function faqatgina positive sonlarni olib
+// string holatida return qilsin.
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12".
+
+function getPositive(arr) {
+    return arr.filter(num => num > 0).join("");
+}
+
+console.log(getPositive([1, -4, 2]));         // "12" bu yerda 1 va 2 musbat son bo'lgani uchun ularni birlashtirib berdi
+// console.log(getPositive([-1, -2, -3]));    // "" bu yerda musbat son yo'q, hammasi manfiy
+// console.log(getPositive([5, 0, 10]));      // "510" barchasi musbat son
+// console.log(getPositive([]));              // "" bu bo'sh array bo'lgani uchun
