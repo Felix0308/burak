970bb7e151920b22bc5fba9f7c88b2780a70315a
Felix0308 <utkirbekm.0327@gmail.com>
2024-12-29 19:20:17 +0900

    feat: N-TASK

--- src/train.ts
+++ src/train.ts
@@ -158,3 +158,11 @@
 // Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, 
 // orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
 // MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
+
+// function palindromCheck(str: string): boolean {
+//     const reversed = str.split('').reverse().join('');
+//     return str === reversed;
+// }
+
+// console.log(palindromCheck("dad"));   // true
+// console.log(palindromCheck("son"));   // false
