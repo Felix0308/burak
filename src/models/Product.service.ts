7588b28acdaa078babab2477d284d8eef51dac9b
Felix0308 <utkirbekm.0327@gmail.com>
2025-01-30 22:01:36 +0900

    fix: modify getProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -9,15 +9,19 @@ import {
 } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { T } from "../libs/types/common";
-import {ObjectId} from "mongoose";
+import { ObjectId } from "mongoose";
+import ViewService from "./View.service";
+import { ViewInput } from "../libs/types/view";
+import { ViewGroup } from "../libs/enums/view.enum";
 
 class ProductService {
   private readonly productModel;
+  public viewService;
 
   constructor() {
     this.productModel = ProductModel;
+    this.viewService = new ViewService();
   }
-
   /** SPA */
 
   public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
@@ -63,31 +67,32 @@ class ProductService {
       })
       .exec();
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
-    // TODO: If authenticated users => first => view log creation
-    // if (memberId) {
-    //   //  Check Existence
-    //   const input: ViewInput = {
-    //     memberId: memberId,
-    //     viewRefId: productId,
-    //     viewGroup: ViewGroup.PRODUCT,
-    //   };
-    //   const existView = await this.viewService.checkViewExistence(input);
 
-    //   console.log("exist:", !!existView);
-    //   if (!existView) {
-    //     // Insert View
-    //     await this.viewService.insertMemberView(input);
+    if (memberId) {
+      //  Check View Log Existence
+      const input: ViewInput = {
+        memberId: memberId,
+        viewRefId: productId,
+        viewGroup: ViewGroup.PRODUCT,
+      };
+      const existView = await this.viewService.checkViewExistence(input);
 
-    //     // Increase Counts
-    //     const result2 = await this.productModel
-    //       .findByIdAndUpdate(
-    //         productId,
-    //         { $inc: { productViews: +1 } },
-    //         { new: true }
-    //       )
-    //       .exec();
-    //   }
-    // }
+      console.log("exist:", !!existView);
+      if (!existView) {
+        // Insert  New View Log
+        await this.viewService.insertMemberView(input);
+
+        // Increase Target Counts
+        const result2 = await this.productModel
+          .findByIdAndUpdate(
+            productId,
+            { $inc: { productViews: +1 } },
+            { new: true }
+          )
+          .exec();
+      }
+    }
+
     return result;
   }
 
