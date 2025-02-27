9cd5ae6827b085407e76fe299329515cea991e3e
Felix0308 <utkirbekm.0327@gmail.com>
2025-02-27 21:27:53 +0900

    feat: ZH-TASK

--- src/train.ts
+++ src/train.ts
@@ -534,8 +534,30 @@
 // Shunday function yozing, u berilgan string parametrni snake casega otkazib qaytarsin. 
 // MASALAN: capitalizeWords('name should be a string') return 'name_should_be_a_string'
 
-function toSnakeCase(str: string): string {
-    return str.toLowerCase().split(' ').join('_');
+// function toSnakeCase(str: string): string {
+//     return str.toLowerCase().split(' ').join('_');
+// }
+
+// console.log(toSnakeCase('name should be a string')); 
+
+// *************************************************************************************************
+// ZH-TASK:
+
+// Shunday function yozing, u berilgan array parametrni ichidagi eng katta 
+// raqamgacha tushib qolgan raqamlarni bir arrayda qaytarsin. 
+// MASALAN: findDisappearedNumbers([1, 3, 4, 7]) return [2, 5, 6]
+
+function findDisappearedNumbers(arr: number[]): number[] {
+    let max = Math.max(...arr);
+    let missing: number[] = [];
+    
+    for (let i = 1; i <= max; i++) {
+        if (!arr.includes(i)) {
+            missing.push(i);
+        }
+    }
+    
+    return missing;
 }
 
-console.log(toSnakeCase('name should be a string')); 
+console.log(findDisappearedNumbers([1, 3, 4, 7]));
